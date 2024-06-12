24db28f0d7ae3c3f6acd8c599d851941f6d09b67
zaooo207 <cailc2364@gmail.com>
2024-06-12 22:44:13 +0700

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -13,7 +13,7 @@ module.exports = {
       const replitLink = "https://replit.com/";
       const youtubeLink = "https://www.youtube.com/";
         const embed = new EmbedBuilder()
-            .setColor('#b300ff')
+            .setColor('#FFB6C1')
             .setAuthor({
               name: 'Support Server',
               iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
