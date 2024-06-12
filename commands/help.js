b10b8ed1e1a50cb6be9682f162d09e5a118ee8b0
zaooo207 <cailc2364@gmail.com>
2024-06-12 23:29:06 +0700

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -13,7 +13,7 @@ module.exports = {
       const embed = new EmbedBuilder()
          .setColor('#FFB6C1')
       .setTitle('<a:nhay:1246437179647856671> Bot của Bún')
-      .setDescription('Chào mừng đến với Music Bot!\n\n<a:music:1246437360749641838> Dưới đây là các lệnh có sẵn:\n\n' +
+      .setDescription('Chào mừng đến với Bé của Bún!\n\n<a:music:1246437360749641838> Dưới đây là các lệnh có sẵn:\n\n' +
         '**/play :** Bắt đầu phát các bài hát.\n' +
         '**/ping :** kiểm tra độ trễ của bot.\n' +
         '**/support :** Hiển thị thông tin máy chủ hỗ trợ');
