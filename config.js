94dfce7dbce49f9bfc66f4d2391e7dd94e9f8b49
zaooo207 <cailc2364@gmail.com>
2024-06-13 23:53:07 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    TOKEN: "",
+    TOKEN: "OTkxMjY5NjI1MTMwMTMxNTE3.GxQPgg.0iugcIIJuBQ0sHCW1dw2bBgETLrmOE43O5tNyo",
     ownerID: ["885126485663449139", "738046846408065045"], 
     setupFilePath: './commands/setup.json',
     commandsDir: './commands',  
